c4c421a86d0f0322518f9790d3ec9f647283bdf3
Jin0207 <86141192+Jin0207@users.noreply.github.com>
2021-12-24 10:27:18 +0900

    BoardDAO 구현

---
+++ test_b/src/main/java/com/sist/dao/BoardDAO.java
@@ -0,0 +1,10 @@
+package com.sist.dao;
+
+public class BoardDAO {
+	public void insert() {
+		System.out.println("등록하였습니다. ");
+	}
+	public void Update() {
+		System.out.println("수정하였습니다. ");
+	}
+}
